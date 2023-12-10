024b94c7e51fc1294331ac3a0cacd85bafd2cbf3
A.Fahmy22 <ahmed.fahmy00@eng-st.cu.edu.eg>
2023-12-10 18:59:08 +0200

    Seven Segment files created in HAL

---
+++ HAL/SEV/SEV_program.c
@@ -0,0 +1,10 @@
+/*****************************************************************/
+/* Author  : Ahmed                                               */
+/* Layer   : HAL                                                 */
+/* SWC     : SEV                                                 */
+/* Version : 1.0                                                 */
+/* Date    : 10 Dec 2023                                         */
+/*****************************************************************/
+#include "SEV_interface.h"
+#include "SEV_private.h"
+#include "SEV_config.h"
