5c7383e0315433f480f881fa35e3e59c5b4bb107
A.Fahmy22 <ahmed.fahmy00@eng-st.cu.edu.eg>
2023-12-30 00:31:18 +0200

    Added USART V1.0

--- MCAL/GIE/GIE_program.c
+++ MCAL/GIE/GIE_program.c
@@ -6,7 +6,7 @@
 /* Date    : 17 Dec 2023                                         */
 /*****************************************************************/
 #include "../../LIB/STD_Types.h"
-#include "../../LIB/Bit_Math.h"
+#include "../../LIB/BIT_Math.h"
 
 #include "GIE_interface.h"
 #include "GIE_private.h"
---
+++ MCAL/USART/USART_interface.h
@@ -0,0 +1,24 @@
+/*****************************************************************/
+/* Author  : Ahmed                                               */
+/* SWC     : USART                                               */
+/* Version : 1.0                                                 */
+/* Date    : 25 Dec 2023                                         */
+/*****************************************************************/
+#ifndef USART_INTERFACE_H_
+#define USART_INTERFACE_H_
+
+void USART_voidInit(void);
+
+tenuErrorStatus USART_enuReceiveSynch(uint16* const pu16DataCpy);
+
+tenuErrorStatus USART_enuTransmitSynch(const uint16 u16DataCpy);
+
+tenuErrorStatus USART_enuGetData(uint16* const pu16DataCpy);
+
+tenuErrorStatus USART_enuSetData(const uint16 u16DataCpy);
+
+tenuErrorStatus USAR_enuReceiveSetCallBack(pf pfReceiveISRCpy);
+
+tenuErrorStatus USAR_enuTransmitSetCallBack(pf pfTransmitISRCpy);
+
+#endif
